228c5875b7ff4a4c9767af97dbc6802c806c2da5
Gerard van Hulzen <gerard.vanhulzen@uhasselt.be>
2022-09-09 11:37:12 +0200

    Improved `response()` code

--- R/response.R
+++ R/response.R
@@ -50,18 +50,24 @@ response_checker.log <- function(log, rule) {
   #                    precedence_type = "eventually_follows") %>%
   #  case_labels() -> holds
 
-  sequence <- paste(rule$activity_a, rule$activity_b, sep = ",")
+  pattern <- paste(rule$activity_a, rule$activity_b, sep = ",")
+
+  # log %>%
+  #   filter_activity(activities = c(rule$activity_a, rule$activity_b)) %>%
+  #   case_list(.keep_trace_list = TRUE) %>%
+  #   rowwise() %>%
+  #   mutate("trace" := list(stringr::str_flatten(trace_list[trace_list != c(trace_list[-1], FALSE)], ","))) %>%
+  #   ungroup() %>%
+  #   mutate("sequence_count" := stringr::str_count(.data[["trace"]], sequence),
+  #          "activity_a_count" := stringr::str_count(.data[["trace"]], rule$activity_a),
+  #          "holds" := if_else(.data[["sequence_count"]] == .data[["activity_a_count"]], TRUE, FALSE)) %>%
+  #   filter(.data[["holds"]]) %>%
+  #   pull(case_id(log)) -> holds
 
   log %>%
-    filter_activity(activities = c(rule$activity_a, rule$activity_b)) %>%
     case_list(.keep_trace_list = TRUE) %>%
-    rowwise() %>%
-    mutate(trace = list(str_flatten(trace_list[trace_list != c(trace_list[-1], FALSE)], ","))) %>%
-    ungroup() %>%
-    mutate(sequence_count = str_count(trace, sequence),
-           activity_a_count = str_count(trace, rule$activity_a),
-           holds = if_else(sequence_count == activity_a_count, TRUE, FALSE)) %>%
-    filter(holds) %>%
+    mutate("holds" := vapply(.data[["trace_list"]], FUN = response_satisfied, FUN.VALUE = logical(1), rule$activity_a, rule$activity_b, pattern)) %>%
+    filter(.data[["holds"]]) %>%
     pull(case_id(log)) -> holds
 
   log %>%
@@ -74,4 +80,20 @@ response_checker.log <- function(log, rule) {
 response_checker.grouped_log <- function(log, rule) {
 
   bupaR:::apply_grouped_fun(log, response_checker.log, rule, .ignore_groups = TRUE, .keep_groups = TRUE, .returns_log = TRUE)
+}
+
+# Checks whether every time A was executed, B was executed after it. B does not have to be executed immediately after A,
+# and another A can be executed between between the first A and subsequent B
+response_satisfied <- function(trace, a, b, pattern) {
+
+  rle_trace <- rle(trace[trace %in% c(a, b)])$values
+  flatten <- stringi::stri_flatten(rle_trace, collapse = ",")
+  sequence_count <- stringi::stri_count_fixed(flatten, pattern)
+  a_count <- length(which(rle_trace == a))
+
+  if (sequence_count == a_count) {
+    return(TRUE)
+  }
+
+  return(FALSE)
 }
--- tests/benchmarks/scratch.R
+++ tests/benchmarks/scratch.R
@@ -1,5 +1,7 @@
 library(dplyr)
 library(tidyr)
+library(stringi)
+library(stringr)
 
 generate_nas <- function (n, pct) {
   data.frame(X = sample.int(1000, n, replace = TRUE),
@@ -23,3 +25,41 @@ results <- bench::press(
       check = TRUE)
   }
 )
+
+
+x <- stri_flatten(stri_rand_strings(100000, 3, "[A-Z]"), ",")
+
+# stri_count(_fixed) is much faster
+bench::mark(
+  str_count = str_count(x, "AAA"),
+  stri_count = stri_count(x, fixed = "AAA"),
+  stri_count_fixed = stri_count_fixed(x, "AAA"),
+  iterations = 1000,
+  check = TRUE
+)
+
+# A tibble: 3 x 13
+#expression            min   median `itr/sec` mem_alloc `gc/sec` n_itr  n_gc total_time result    memory              time               gc
+#<bch:expr>       <bch:tm> <bch:tm>     <dbl> <bch:byt>    <dbl> <int> <dbl>   <bch:tm> <list>    <list>              <list>             <list>
+#1 str_count         831.8us    999us      930.    16.3KB        0  1000     0      1.07s <int [1]> <Rprofmem [23 x 3]> <bench_tm [1,000]> <tibble [1,000 x 3]>
+#2 stri_count         34.6us   36.9us    25603.        0B        0  1000     0    39.06ms <int [1]> <Rprofmem [0 x 3]>  <bench_tm [1,000]> <tibble [1,000 x 3]>
+#3 stri_count_fixed   30.7us   34.4us    24735.        0B        0  1000     0    40.43ms <int [1]> <Rprofmem [0 x 3]>  <bench_tm [1,000]> <tibble [1,000 x 3]>
+
+
+x <- stri_rand_strings(100000, 3, "[A-Z]")
+
+# which is slightly faster than vector, but much faster than rle
+bench::mark(
+  vector = length(x[x == "AAA"]),
+  rle = sum(rle(x == "AAA")$values),
+  which = length(which(x == "AAA")),
+  iterations = 1000,
+  check = TRUE
+)
+
+# A tibble: 3 x 13
+#expression      min   median `itr/sec` mem_alloc `gc/sec` n_itr  n_gc total_time result    memory              time               gc
+#<bch:expr> <bch:tm> <bch:tm>     <dbl> <bch:byt>    <dbl> <int> <dbl>   <bch:tm> <list>    <list>              <list>             <list>
+#1 vector      506.6us  766.2us     1048.  781.35KB     25.8   976    24   931.39ms <int [1]> <Rprofmem [2 x 3]>  <bench_tm [1,000]> <tibble [1,000 x 3]>
+#2 rle          1.76ms    3.5ms      292.    4.96MB     63.3   822   178      2.81s <int [1]> <Rprofmem [13 x 3]> <bench_tm [1,000]> <tibble [1,000 x 3]>
+#3 which       501.8us  715.3us     1218.  781.35KB     31.2   975    25    800.2ms <int [1]> <Rprofmem [2 x 3]>  <bench_tm [1,000]> <tibble [1,000 x 3]>
